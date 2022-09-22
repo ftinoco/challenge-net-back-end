1c9210459186756d2bddd10afcca6eeca2c23e8d
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-09-22 16:09:24 -0600

    Creating GetUserById endpoint

---
+++ ChallengeNetBackEnd/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using ChallengeNetBackEnd.Data.Interfaces;
+using ChallengeNetBackEnd.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeNetBackEnd.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public readonly IMapper _mapper;
+        private readonly IUserQueries _queries;
+
+        public UsersController(
+            IUserQueries queries,
+            IMapper mapper)
+        {
+            _queries = queries;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public UserDTO GetUserById(int id)
+        {
+            var user = _queries.GetUserById(id);
+            return _mapper.Map<UserDTO>(user);
+        }
+    }
+}

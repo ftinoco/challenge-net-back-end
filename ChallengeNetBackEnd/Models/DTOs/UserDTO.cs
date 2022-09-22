160a820de14b94eb79e15d947a077d6f5532fe50
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-09-22 15:46:32 -0600

    Updating user entity and dto

--- ChallengeNetBackEnd/Models/DTOs/UserDTO.cs
+++ ChallengeNetBackEnd/Models/DTOs/UserDTO.cs
@@ -2,5 +2,9 @@
 {
     public class UserDTO
     {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string AdvisorFullname { get; set; }
+        public DateTime CreationDate { get; set; }
     }
 }
--- ChallengeNetBackEnd/Models/User.cs
+++ ChallengeNetBackEnd/Models/User.cs
@@ -1,6 +1,25 @@
-﻿namespace ChallengeNetBackEnd.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChallengeNetBackEnd.Models
 {
+    [Table("user", Schema = "public")]
     public class User
     {
+
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("firstname")]
+        public string FirstName { get; set; }
+        [Column("surname")]
+        public string Surname { get; set; }
+        [Column("created")]
+        public DateTime Created { get; set; }
+        [Column("modified")]
+        public DateTime Modified { get; set; }
+        [Column("currencyid")]
+        public int CurrencyId { get; set; }
+
+        [ForeignKey("advisorid")]
+        public virtual User? Advisor { get; set; }
     }
 }

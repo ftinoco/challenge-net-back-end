2966a6f964d365cd52440cc7ba98bcf0a44fd918
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-09-22 15:26:38 -0600

    Removing controller auto generated by VS and adding controller for testing connections

---
+++ ChallengeNetBackEnd/Controllers/DefaultController.cs
@@ -0,0 +1,31 @@
+﻿using ChallengeNetBackEnd.Data.Implementations;
+using ChallengeNetBackEnd.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeNetBackEnd.Controllers
+{ 
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        private readonly IDataContext _context;
+
+        public DefaultController(IDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("ping")]
+        public string ping()
+        {
+            return "pong";
+        }
+
+        [HttpGet("canConnect")] 
+        public string DbConnection()
+        {
+            if (((DataContext)_context).Database.CanConnect())
+                return "connected!";
+            return "without connection!";
+        }
+    }
+}

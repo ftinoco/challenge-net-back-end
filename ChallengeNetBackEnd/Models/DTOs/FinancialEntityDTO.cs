b3c8828b64d1a306f7da539cc9dcc8a947a1dd43
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-09-22 23:23:59 -0600

    Adding financial entity model and dto

---
+++ ChallengeNetBackEnd/Models/DTOs/FinancialEntityDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ChallengeNetBackEnd.Models.DTOs
+{
+    public class FinancialEntityDTO
+    {
+        public string Logo { get; set; }
+        public string Title { get; set; }
+        public string Uuid { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ ChallengeNetBackEnd/Models/FinancialEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ChallengeNetBackEnd.Models
+{
+    [Table("financialentity", Schema = "public")]
+    public class FinancialEntity: EntityBase
+    {
+        [Column("logo")]
+        public string Logo { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("uuid")]
+        public string Uuid { get; set; }
+        [Column("description")]
+        public string Description { get; set; }
+        
+    }
+}

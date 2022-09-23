e7b09061d0a05629ab91f1f5344a664f00359367
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-09-23 17:00:39 -0600

    Adding helper class

---
+++ ChallengeNetBackEnd/Utils/Helpers.cs
@@ -0,0 +1,24 @@
+﻿namespace ChallengeNetBackEnd.Utils
+{
+    public class Helpers
+    {
+        public static string ParsingStringToDate(string date, out DateTime output)
+        {
+            string msg = string.Empty;
+            output = DateTime.Now;
+            try
+            {
+                string[] arrDate = date.Split('-');
+                if (arrDate.Length > 1)
+                    output = new DateTime(int.Parse(arrDate[2]), int.Parse(arrDate[1]), int.Parse(arrDate[0])); 
+                else
+                    msg = "Date must have the following format {dd-mm-yyyy}";
+            }
+            catch (Exception)
+            {
+                msg = "Date must have valid values";
+            }
+            return msg;
+        }
+    }
+}

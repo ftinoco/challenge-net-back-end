781cda3b2c893060eb02704ec1e1a737351ea7ad
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-09-23 22:30:47 -0600

    Adding model and dto

---
+++ ChallengeNetBackEnd/Models/DTOs/GoalDetailDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ChallengeNetBackEnd.Models.DTOs
+{
+    public class GoalDetailDTO: GoalDTO
+    {
+        public string GoalCategory { get; set; }
+        public double Contributions { get; set; }
+        public double Withdrawal { get; set; }
+        public double TargetAmountPercentage { get; set; }
+    }
+}
